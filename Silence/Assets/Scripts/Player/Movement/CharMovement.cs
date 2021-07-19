15ff137e483cf76f96d6a68aa358030c3b6c7091
Rob <robert.a.nickel@gmail.com>
2021-07-19 15:25:29 +1200

    Script Setup
    Starting planning camera script
    Added New Input system
    Started setting up inputs

---
+++ Silence/Assets/Scripts/Player/Movement/CharMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class CharMovement : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveInput(CallbackContext context)
+    {
+
+    }
+
+    public void RepositionCamera(CallbackContext context)
+    {
+
+    }
+}
