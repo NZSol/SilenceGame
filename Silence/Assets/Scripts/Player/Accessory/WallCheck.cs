2224d0c58e51976df464813a57442a23083fac3b
Rob <robert.a.nickel@gmail.com>
2021-08-06 17:26:08 +1200

    more stuff

---
+++ Silence/Assets/Scripts/Player/Accessory/WallCheck.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCheck : MonoBehaviour
+{
+    [SerializeField] LayerMask wallLayer;
+    [SerializeField] float range = 0.5f;
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, range, wallLayer))
+        {
+            Debug.DrawRay(transform.position, transform.forward * range, Color.green);
+        }
+    }
+}
